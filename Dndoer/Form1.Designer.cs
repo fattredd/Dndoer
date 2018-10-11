8fa129687d8e2d98c6d9c925326a8513c3c3a41f
JButler <jack.butler@bwpapersystems.com>
2018-10-11 13:42:17 -0400

    Added line parsing, complex functions

--- Dndoer/Form1.Designer.cs
+++ Dndoer/Form1.Designer.cs
@@ -31,18 +31,17 @@ namespace Dndoer
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.d20RollBttn = new System.Windows.Forms.Button();
             this.resultD20 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.qtD20 = new System.Windows.Forms.NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.qtD20)).BeginInit();
+            this.d20Image = new System.Windows.Forms.PictureBox();
+            this.DiceInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.d20Image)).BeginInit();
             this.SuspendLayout();
             // 
             // d20RollBttn
             // 
             this.d20RollBttn.Font = new System.Drawing.Font("Arial Unicode", 12F);
-            this.d20RollBttn.Location = new System.Drawing.Point(142, 94);
+            this.d20RollBttn.Location = new System.Drawing.Point(142, 98);
             this.d20RollBttn.Name = "d20RollBttn";
-            this.d20RollBttn.Size = new System.Drawing.Size(75, 30);
+            this.d20RollBttn.Size = new System.Drawing.Size(75, 29);
             this.d20RollBttn.TabIndex = 1;
             this.d20RollBttn.Text = "Roll D20";
             this.d20RollBttn.UseVisualStyleBackColor = true;
@@ -52,53 +51,47 @@ namespace Dndoer
             // 
             this.resultD20.AutoSize = true;
             this.resultD20.Font = new System.Drawing.Font("Arial Unicode", 12F);
-            this.resultD20.Location = new System.Drawing.Point(127, 40);
+            this.resultD20.Location = new System.Drawing.Point(127, 39);
             this.resultD20.Name = "resultD20";
             this.resultD20.Size = new System.Drawing.Size(90, 21);
             this.resultD20.TabIndex = 2;
             this.resultD20.Text = "No Roll Yet";
             // 
-            // pictureBox1
+            // d20Image
             // 
-            this.pictureBox1.Image = global::Dndoer.Properties.Resources.twenty_sided_dice;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 11);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(109, 77);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.d20Image.Image = global::Dndoer.Properties.Resources.twenty_sided_dice;
+            this.d20Image.Location = new System.Drawing.Point(12, 11);
+            this.d20Image.Name = "d20Image";
+            this.d20Image.Size = new System.Drawing.Size(109, 77);
+            this.d20Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.d20Image.TabIndex = 3;
+            this.d20Image.TabStop = false;
+            this.d20Image.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // qtD20
+            // DiceInput
             // 
-            this.qtD20.AllowDrop = true;
-            this.qtD20.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.qtD20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.qtD20.Font = new System.Drawing.Font("Arial Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.qtD20.Location = new System.Drawing.Point(12, 94);
-            this.qtD20.Name = "qtD20";
-            this.qtD20.Size = new System.Drawing.Size(124, 29);
-            this.qtD20.TabIndex = 0;
-            this.qtD20.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
+            this.DiceInput.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.DiceInput.Font = new System.Drawing.Font("Arial Unicode", 12F);
+            this.DiceInput.Location = new System.Drawing.Point(12, 98);
+            this.DiceInput.Name = "DiceInput";
+            this.DiceInput.Size = new System.Drawing.Size(109, 29);
+            this.DiceInput.TabIndex = 4;
+            this.DiceInput.Text = "1d20+2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(229, 139);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.DiceInput);
+            this.Controls.Add(this.d20Image);
             this.Controls.Add(this.resultD20);
             this.Controls.Add(this.d20RollBttn);
-            this.Controls.Add(this.qtD20);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "DnDoer";
             this.TransparencyKey = System.Drawing.Color.White;
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.qtD20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d20Image)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,8 +100,8 @@ namespace Dndoer
         #endregion
         private System.Windows.Forms.Button d20RollBttn;
         private System.Windows.Forms.Label resultD20;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.NumericUpDown qtD20;
+        private System.Windows.Forms.PictureBox d20Image;
+        private System.Windows.Forms.TextBox DiceInput;
     }
 }
 
--- Dndoer/Form1.cs
+++ Dndoer/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace Dndoer
 {
@@ -20,9 +21,62 @@ namespace Dndoer
 
         private void d20RollBttn_Click(object sender, EventArgs e)
         {
-            int maxNum = (int)qtD20.Value * 20;
-            int minNum = (int)qtD20.Value;
-            resultD20.Text = rnd.Next(minNum, maxNum).ToString();
+            string reg = "([\\+|\\-| ])";
+            string[] dice = Regex.Split(DiceInput.Text, reg);
+
+            int temp = 0;
+            int result = 0;
+            int operation = 0; // 0 is add, 1 is sub
+
+            foreach (string item in dice)
+            {
+                // Run the operation
+                if (operation == 0) { result += temp; }
+                else if (operation == 1) { result -= temp; }
+
+                string clean = item.Trim();
+                if (int.TryParse(clean, out temp)) // If it's a number
+                {
+                    // Then that's what it is
+
+                } else if (clean.IndexOf('d') != -1) // If it's a dice roll
+                {
+                    // Roll the dice
+                    string[] dieArray = clean.Split('d');
+                    dieArray[0] = (dieArray[0] == "" ? "1" : dieArray[0]);
+                    int[] die = Array.ConvertAll(dieArray, num => int.Parse(num));
+
+                    int maxNum = (int)die[1] * die[0];
+                    int minNum = (int)die[0];
+                    temp = rnd.Next(minNum, maxNum);
+
+                } else if (Regex.Match(clean, reg).Success) // If it's an operation
+                {
+                    // Set the operation
+                    if (clean == "+") { operation = 0;  }
+                    else if (clean == "-") { operation = 1; }
+                    else { operation = 0; }
+                    temp = 0;
+                }
+                Console.WriteLine(temp);
+            }
+
+            // Run the operation one last time
+            if (operation == 0) { result += temp; }
+            else if (operation == 1) { result -= temp; }
+
+            resultD20.Text = result.ToString();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (DiceInput.Text == "1d20+2")
+            {
+                DiceInput.Text = "1d20";
+            } else
+            {
+                DiceInput.Text = "1d20+2";
+            }
         }
     }
 }
